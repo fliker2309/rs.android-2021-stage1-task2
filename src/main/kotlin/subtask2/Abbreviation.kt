fd94937ffab67ef25969c4f00efe7ec232aebad7
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-06-13 21:53:21 +0300

    add: second task without test 3

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,15 +1,17 @@
 package subtask2
 
+import java.util.regex.Pattern
+
 class Abbreviation {
 
 
     fun abbreviationFromA(a: String, b: String): String {
-
-
+        //b - regex
+        //tempA string
         val tempA = a.toUpperCase()
+        val pattern = Pattern.compile(b)
+        val matcher = pattern.matcher(tempA)
 
-        val matchResult = Regex(tempA).find(b) ?: return "NO"
-        return matchResult.toString()
-
+        return if (matcher.find()) "YES" else "NO"
     }
 }
--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,12 @@
-package subtask3
-
-import kotlin.reflect.KClass
-
-class Blocks {
-
-
-    fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-  //TODO
-    }
-}
+//package subtask3
+//
+//import kotlin.reflect.KClass
+//
+//class Blocks {
+//
+//
+//    fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
+//  //TODO
+//        return //TODO
+//    }
+//}
