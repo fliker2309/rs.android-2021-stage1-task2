84a3bc057fd315a1a0e40bad5e76be8d929541d5
Evgenii <60140529+fliker2309@users.noreply.github.com>
2021-06-15 17:48:06 +0300

    add: task 3 completed

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,45 @@
 package subtask3
 
+import java.time.LocalDate
 import kotlin.reflect.KClass
 
 class Blocks {
 
 
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+
+        when (blockB) {
+            Int::class -> {
+                var count = 0
+                blockA.forEach {
+                    if (it is Int) {
+                        count += it
+                    }
+                }
+                return count
+            }
+            String::class -> {
+                var text = ""
+                blockA.forEach {
+                    if (it is String) {
+                        text += it
+                    }
+                }
+                return text
+            }
+            LocalDate::class -> {
+                var date: LocalDate = LocalDate.ofEpochDay(0)
+                blockA.forEach {
+                    if (it is LocalDate) {
+                        if (it > date) date = it
+                    }
+                }
+                return "${date.dayOfMonth}.${date.monthValue}.${date.year}"
+            }
+            else -> {
+                return false
+            }
+        }
+
     }
 }
